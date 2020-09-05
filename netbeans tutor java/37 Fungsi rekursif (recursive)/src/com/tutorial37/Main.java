0c324b23256d7b8f5843cb040046f548f0dc0b92
Ganes <ghanes556@gmail.com>
2020-09-05 21:24:22 +0800

    new

--- netbeans tutor java/37 Fungsi rekursif (recursive)/src/com/tutorial37/Main.java
+++ netbeans tutor java/37 Fungsi rekursif (recursive)/src/com/tutorial37/Main.java
@@ -17,7 +17,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2");
+        
     }
     
 }
