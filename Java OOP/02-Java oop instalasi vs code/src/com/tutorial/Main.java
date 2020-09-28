35998149021231e452cbc8753ecca226f94016ee
Ganes <ghanes556@gmail.com>
2020-09-28 14:51:52 +0800

    update

--- Java OOP/02-Java oop instalasi vs code/src/com/tutorial/Main.java
+++ Java OOP/02-Java oop instalasi vs code/src/com/tutorial/Main.java
@@ -1,7 +1,7 @@
 package com.tutorial;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\nide dari java menggunakan vscode");
+        System.out.println("\n\nide dari java menggunakan vscode"); // contoh
         
     }
 }
