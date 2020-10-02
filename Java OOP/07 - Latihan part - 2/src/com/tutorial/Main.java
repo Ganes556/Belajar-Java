df34357dc19dd23a8e9b6af61702d40c46a9412c
ganes <gustiganes@gmail.com>
2020-10-02 17:50:21 +0800

    update

--- Java OOP/07 - Latihan part - 2/src/com/tutorial/Main.java
+++ Java OOP/07 - Latihan part - 2/src/com/tutorial/Main.java
@@ -15,6 +15,24 @@ class Player{
         this.health = health;
     }
 
+    void attack(Player opponent){
+        double attackPower = this.weapon.attackPower;
+        String attack = this.name + " attacking " + opponent.name + " with power: " + attackPower;        
+        System.out.println(attack);
+        // defence untuk si player inputnya
+        opponent.defence(attackPower);
+    }
+    void defence(double attackPower){
+        double damage;
+        if (attackPower > this.armor.defence){
+            damage = attackPower - this.armor.defence;
+        }else{
+            damage = 0;
+        }
+        this.health -= damage;
+        System.out.println(this.name + " gets damage: " + damage);
+        System.out.printf("Sisa Health %s : %.1f\n",this.name,this.health);
+    }
     void equipWeapon(Weapon weapon){
         this.weapon = weapon;
     }
@@ -34,17 +52,17 @@ class Player{
 }
 // Senjata
 class Weapon{
-    double damage;
+    double attackPower;
     String weaponName;
-    Weapon(String weaponName, double damage){
-        this.damage = damage;
+    Weapon(String weaponName, double attackPower){
+        this.attackPower = attackPower;
         this.weaponName = weaponName;
     }
     void display(){
-        System.out.println("Weapon : " + this.weaponName + ", damage : " + this.damage);
+        System.out.println("Weapon : " + this.weaponName + ", attackPower : " + this.attackPower);
     }
 }
-// Armor
+// Armor 
 class Armor{
     double defence;
     String armorName;
@@ -60,12 +78,12 @@ class Armor{
 
 public class Main {
     public static void main(String[] args) { // jika berisi String[] args baru bisa di panggil dengan "java Main.java"
-                                             // jika tidak ya gk bsa di panggil baik mudah jadi gunakan IDE anjk
+                                             // jika tidak ya gk bsa di panggil baik mudah jadi gunakan command "java -cp <class path>"
         // membuat object player 
         Player player1 = new Player("MegaCUP",3000);
         Player player2 = new Player("Asadin'M",500);
         // membuat object weapon
-        Weapon sword = new Weapon("Sword",10);
+        Weapon sword = new Weapon("Sword",20);
         Weapon knife = new Weapon("Knife",65);
         // membuat object armor
         Armor harness = new Armor("Harness",40);
@@ -74,12 +92,18 @@ public class Main {
         // player 1
         player1.equipWeapon(sword);
         player1.equipArmor(harness);
-        player1.display();
+        // player1.display();
 
         // player2
         player2.equipWeapon(knife);
         player2.equipArmor(assasinArmor);
-        player2.display();
-        System.out.println("================================");
+
+        System.out.println("\nPERTEMPURAN");
+        System.out.println("===============\nTurn - 1\n");
+        player1.attack(player2);         
+        System.out.println("===============\nTurn - 2\n");
+        player2.attack(player1);
+
+        System.out.println("===============");
     }
 }
