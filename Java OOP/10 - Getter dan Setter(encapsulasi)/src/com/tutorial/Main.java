ddb203e15e28bec0ea4d7c5f9f2ce0bf065fdc09
ganes <gustiganes@gmail.com>
2020-10-05 19:49:23 +0800

    update main

--- Java OOP/10 - Getter dan Setter(encapsulasi)/src/com/tutorial/Main.java
+++ Java OOP/10 - Getter dan Setter(encapsulasi)/src/com/tutorial/Main.java
@@ -3,16 +3,47 @@ package com.tutorial;
 class Data{
     public int intPublic;
     private int intPrivate;
-
+    private double doublePrivate;
     public Data(){
-        this.intPrivate = 0;
-        this.intPrivate = 0;
+        this.intPublic = 0;
+        this.intPrivate = 10;
     }
 
     // getter
     public int getIntPrivate(){
         return intPrivate;
     }
+    // setter
+    public void setDoublePrivate(double value){
+        this.doublePrivate = value;
+    }
+    public double getDoublePrivate() {
+        return this.doublePrivate;
+    }
+}
+
+class Lingkaran{
+    private double diameter;
+
+    Lingkaran(double diameter){
+        this.diameter = diameter;
+    }
+
+    // setter 
+    public void setDiameter(double jari2) {
+        this.diameter = jari2 *2;
+    }
+
+    // getter
+    public double getJari2(){
+        return this.diameter/2;
+    }
+
+    // getter 
+    public double getLuas(){
+        return 3.14* Math.pow(this.getJari2(), 2);
+    }
+
 }
 
 public class Main {
@@ -21,10 +52,25 @@ public class Main {
         Data object = new Data();
 
         // read and write dengan menggunakan public
-        object.intPublic = 10; // write
-        System.out.println(object.intPublic); // read
+        object.intPublic = 5; // write
+        System.out.println("public : " + object.intPublic); // read
 
         // read only (menggunakan getter)
-        System.out.println(object.getIntPrivate());        
+        int angka = object.getIntPrivate();
+        System.out.println("getter : " + angka);  
+              
+
+        // write only (hanya bisa menulis, dengan menggunakan setter)
+        object.setDoublePrivate(10.5);       
+        System.out.println("setter : " + object.getDoublePrivate());
+        
+        // setter jari-jari dari diameter
+        Lingkaran object2 = new Lingkaran(5);
+        System.out.println("Jari-jari : " + object2.getJari2());
+        object2.setDiameter(14); // diameter
+        System.out.println("Jari-jari : " + object2.getJari2());
+
+        // Luas lingkaran
+        System.out.println(object2.getLuas());
     }
 }
